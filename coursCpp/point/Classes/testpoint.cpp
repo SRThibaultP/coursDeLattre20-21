c1044f01fc0774fb9383088459a8351686386538
ThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2020-09-08 14:20:41 +0200

    C++ : TP

--- coursCpp/point/Classes/testpoint.cpp
+++ coursCpp/point/Classes/testpoint.cpp
@@ -8,11 +8,6 @@ p->Init(3,4);
 p->Affiche();
 p->Deplace(4,6);
 p->Affiche();
-
-p->x = 25; // Accès aux variables de la classe point
-p ->y = p-> x + 10;
-cout << "le point est en " << p->x << ", " << p->y << endl;
-p -> Affiche() ;
 delete p ;
 return 0;
 }
