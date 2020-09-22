2ef96794df8e2f059e1ddfe179cf1a000aa44705
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2020-09-22 11:40:59 +0200

    Update from 22/09 - 11:41

--- coursCpp/point/Classes/point.h
+++ coursCpp/point/Classes/point.h
@@ -2,7 +2,7 @@
 using namespace std ;
 #ifndef CLASSES_POINT
 #define CLASSES_POINT
-
+/*
 class Point
 {
 private :
@@ -30,6 +30,36 @@ int GetY();
 void SetX(int);
 void SetY(int);
 
+};
+*/
+
+class Point
+{
+protected :
+int x;
+int y;
+public :
+//Point(int abs, int ord);
+Point(int a=0, int b=0) ;
+Point(Point & pt) ;// Constructeur par recopie
+Point operator+(const Point & a);
+Point& operator=(const Point & a);
+bool operator==(const Point & a);
+Point& operator+=( const Point & a);
+bool Coincide(Point & p);
+//Point();
+//Point(int,int);
+~Point();
+void Init(int a, int b) ;
+void Init(int a); // Initialisation avec une même valeur
+void Deplace(int a, int b) ;
+void Deplace(int a);
+void Affiche() ;
+void Affiche(char* strMesg); // Affichage avec un message
+int GetX();
+int GetY();
+void SetX(int);
+void SetY(int);
 };
 
 #endif /* CLASSES_POINTCOL_H_ */
