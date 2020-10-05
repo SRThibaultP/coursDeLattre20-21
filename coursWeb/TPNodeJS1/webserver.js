7745f3344cb16755eb89427815e57da6842fb2fb
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2020-10-05 10:36:00 +0200

    NodeJS : Fin du TP (vraiment)

--- coursWeb/TPNodeJS1/webserver.js
+++ coursWeb/TPNodeJS1/webserver.js
@@ -1,8 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function (request, response) {
-  response.write('<p>Bonjour les &eacute;tudiants</p>');
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(8080);
+});
+
+var server81 = http.createServer(function (request, response) {
+  //request.setHeader()
+  response.write('<p>Bonjour les &eacute;tudiants en 8081</p>');
   response.end();
 }
 );
-server.listen(8080);
+server81.listen(8081);
--- coursWeb/TPNodeJS1/webserver8081.js
+++ coursWeb/TPNodeJS1/webserver8081.js
@@ -1,9 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function (request, response) {
-  //request.setHeader()
-  response.write('<p>Bonjour les &eacute;tudiants en 8081</p>');
-  response.end();
-}
-);
-server.listen(8081);
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(8081);
+});
